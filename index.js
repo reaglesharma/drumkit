9cb694104745044727650afd14b8433a8d76b977
Ajay Sharma <83692316+reaglesharma@users.noreply.github.com>
2022-06-13 16:17:58 +0530

    updated js file

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 function makeSound(key) {
   switch (key) {
     case "w":
-      var action = new Audio("sounds/tom-1.mp3");
+      var action = new Audio("tom-1.mp3");
       action.play();
       break;
     case "a":
-      var action = new Audio("sounds/tom-2.mp3");
+      var action = new Audio("tom-2.mp3");
       action.play();
       break;
     case "s":
-      var action = new Audio("sounds/tom-3.mp3");
+      var action = new Audio("tom-3.mp3");
       action.play();
       break;
     case "d":
-      var action = new Audio("sounds/tom-4.mp3");
+      var action = new Audio("tom-4.mp3");
       action.play();
       break;
     case "j":
-      var action = new Audio("sounds/snare.mp3");
+      var action = new Audio("snare.mp3");
       action.play();
       break;
     case "k":
-      var action = new Audio("sounds/crash.mp3");
+      var action = new Audio("crash.mp3");
       action.play();
       break;
     case "l":
-      var action = new Audio("sounds/kick-bass.mp3");
+      var action = new Audio("kick-bass.mp3");
       action.play();
       break;
     default:
